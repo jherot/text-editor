814b249a3834b14ecd24ee71921cf5d676cbe7f8
Jacob Herot <jmherot@icloud.com>
2023-12-23 18:46:41 -0500

    Change some formatting, create global editor state

--- flux.c
+++ flux.c
@@ -15,7 +15,11 @@
 
 /*** data ***/
 
-struct termios orig_termios;
+struct editorConfig {
+  struct termios orig_termios;
+};
+
+struct editorConfig E;
 
 
 /*** terminal ***/
@@ -29,15 +33,15 @@ void die(const char *s) {
 }
 
 void disableRawMode() {
-  if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1)
+  if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &E.orig_termios) == -1)
     die("tcsetattr");
 }
 
 void enableRawMode() {
-  if (tcgetattr(STDIN_FILENO, &orig_termios) == -1) die("tcgetattr");
+  if (tcgetattr(STDIN_FILENO, &E.orig_termios) == -1) die("tcgetattr");
   atexit(disableRawMode);
 
-  struct termios raw = orig_termios;
+  struct termios raw = E.orig_termios;
   raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
   raw.c_oflag &= ~(OPOST);
   raw.c_cflag |= (CS8);
@@ -52,9 +56,9 @@ char editorReadKey() {
   int nread;
   char c;
   while ((nread = read(STDIN_FILENO, &c, 1)) != 1) {
-      if  (nread == -1 && errno != EAGAIN) die("read");
-    }
-    return c;
+    if  (nread == -1 && errno != EAGAIN) die("read");
+  }
+  return c;
 }
 
 
