aade7e47678430025e89c5068947d78476bd89b8
Jacob Herot <jmherot@icloud.com>
2023-12-23 18:38:40 -0500

    Clear screen and reposition cursor on exit

--- flux.c
+++ flux.c
@@ -21,6 +21,9 @@ struct termios orig_termios;
 /*** terminal ***/
 
 void die(const char *s) {
+  write(STDOUT_FILENO, "\x1b[2J", 4);
+  write(STDOUT_FILENO, "\x1b[H", 3);
+  
   perror(s);
   exit(1);
 }
@@ -55,6 +58,14 @@ char editorReadKey() {
 }
 
 
+/*** output ***/
+
+void editorRefreshScreen() {
+  write(STDOUT_FILENO, "\x1b[2J", 4);
+  write(STDOUT_FILENO, "\x1b[H", 3);
+}
+
+
 /*** input ***/
 
 void editorProcessKeypress() {
@@ -62,6 +73,8 @@ void editorProcessKeypress() {
 
   switch (c) {
     case CTRL_KEY('q'):
+      write(STDOUT_FILENO, "\x1b[2J", 4);
+      write(STDOUT_FILENO, "\x1b[H", 3);
       exit(0);
       break;
   }
@@ -74,6 +87,7 @@ int main() {
   enableRawMode();
   
   while (1) {
+    editorRefreshScreen();
     editorProcessKeypress();
   }
   
