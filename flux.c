9e7efd5d3cc31428aa999ffc03c7899b35a27fd2
Jacob Herot <jmherot@icloud.com>
2023-12-23 23:36:40 -0500

    Limit scrolling to right

--- flux.c
+++ flux.c
@@ -286,7 +286,8 @@ void editorRefreshScreen() {
   editorDrawRows(&ab);
 
   char buf[32];
-  snprintf(buf, sizeof(buf), "\x1b[%d;%dH", (E.cy - E.rowoff) + 1, E.cx + 1);
+  snprintf(buf, sizeof(buf), "\x1b[%d;%dH", (E.cy - E.rowoff) + 1,
+	                                    (E.cx - E.coloff) + 1);
   abAppend(&ab, buf, strlen(buf));
   
   abAppend(&ab, "\x1b[?25h", 6);
@@ -299,12 +300,14 @@ void editorRefreshScreen() {
 /*** input ***/
 
 void editorMoveCursor(int key) {
+  erow *row = (E.cy >= E.numrows) ? NULL : &E.row[E.cy];
+  
   switch(key) {
     case ARROW_LEFT:
       if (E.cx != 0) E.cx--;
       break;
     case ARROW_RIGHT:
-      E.cx++;
+      if (row && E.cx < row->size) E.cx++;
       break;
     case ARROW_UP:
       if (E.cy != 0) E.cy--;
